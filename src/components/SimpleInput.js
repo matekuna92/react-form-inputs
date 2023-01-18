9ed62e4b4949220b100a50bf8738992ed8502ea1
matekuna92 <kunamate66@gmail.com>
2023-01-18 22:51:15 +0100

    basic validation which can cause problems

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,6 +2,7 @@ import { useState, useRef } from 'react'
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
+  const [nameIsValid, setNameIsValid] = useState(true);
   const nameInputRef = useRef();
 
   const nameChangeHandler = (event) => {
@@ -12,14 +13,23 @@ const SimpleInput = (props) => {
   const formSubmitHandler = (event) => {
     event.preventDefault();
 
+    if(enteredName.trim() === '') {
+		setNameIsValid(false);
+		return;
+    }
+
+	setEnteredName(true);
     console.log(enteredName);
   }
 
+  const nameInputClasses = `form-control ${!nameIsValid ? 'invalid' : ''}`;
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' onChange={nameChangeHandler} ref={nameInputRef}/>
+		{!nameIsValid && <p className='error-text'> Name must not be empty. </p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
